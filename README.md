5899e2e3deae8f35ed7e22f64a3cbbe960615cbd
Jeremy Carlsten <jcc.993@gmail.com>
2022-01-07 12:57:04 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#Remix - A new react framework
+
+- talk about what's diferent than generated.
+    - npm i
+    - git init / commit
+    - small code removal 
+    - This readme
+
+## What is remix?
+    A framework wraped around react, react-router and the server AKA "mix"
+
+    Pros: 
+        - Follows closely with with the w3c / MDN standards
+        - Progressive enhancement
+        - nested routes 
+    Cons: 
+        - she's opinonated
+        - VERY new. V1 released late Nov 2021. 
---
+++ app/root.jsx
@@ -0,0 +1,27 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+} from "remix";
+
+export function meta() {
+  return { title: "New Remix App" };
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
