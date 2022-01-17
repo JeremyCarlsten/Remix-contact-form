c8bfbaa1d4c1878c294a5d7df7aaff2dc0a969ed
Jeremy Carlsten <jcc.993@gmail.com>
2022-01-17 13:48:22 -0600

    adding projects to list

--- app/routes/projects/index.jsx
+++ app/routes/projects/index.jsx
@@ -1,3 +1,25 @@
+import { Link } from 'remix';
+
+const totalyLegitProjects = [
+    { id: 1, name: 'Youtub' },
+    { id: 2, name: 'Facespace' },
+    { id: 3, name: 'Teller' },
+    { id: 4, name: 'InstaPics' }
+];
+
 export default function ProjectsRoute() {
-    return (<h1>Projects</h1>)
+    return (
+        <div className="projects">
+            <h1>Projects</h1>
+            <ul className="projects-list">
+                {totalyLegitProjects.map((project) => {
+                    return (
+                        <li key={project.id}>
+                            <Link to={`/projects/${project.name}`}>{project.name}</Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
 }
